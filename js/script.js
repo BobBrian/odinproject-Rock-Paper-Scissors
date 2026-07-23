cd5e8d70ae58a74717a51f1d30f13c2870e46fbe
BobBrian <rellrocker235@gmail.com>
2026-07-23 10:33:29 -0230

    Updated getComputerChoice: initalized each choice within the function and used switch to map the numbers generated by Math.Random to a variable

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-function getComputerChoice (rock,paper,scissors ) {
-    // Math.random() *3 is used so that we keep our range to 0 to 2.99
-    // Math.floor() is used to round down our options so that we only have 0 , 1, 2
+function getComputerChoice () {
+    let A = "rock"
 
-    const randomIndex = Math.floor(Math.random() * 3)
+    let B = "paper"
 
-    if (randomIndex == 0) return rock
+    let C = "scissors"
 
-    if (randomIndex == 1) return paper
+    let randomNumber = Math.floor(Math.random()*3)
 
-    if (randomIndex == 2) return scissors
-
+    // Map each number to a variable
+    switch(randomNumber){
+        case 0:
+            return A;
+        case 1:
+            return B;
+        case 2:
+            return C;
+    }
 }
