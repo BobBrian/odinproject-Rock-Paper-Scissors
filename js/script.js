805d17a1f6f9c3d44180ee3363696c4e27930d3c
BobBrian <rellrocker235@gmail.com>
2026-07-23 20:05:15 -0230

    Modified switch stament in getComputerChoice so that each random number is assigned a string withouht the need to initalize a variable first

--- js/script.js
+++ js/script.js
@@ -3,22 +3,17 @@ let computerScore = 0;
 let round = 5;
 
 function getComputerChoice () {
-    let A = "rock"
-
-    let B = "paper"
-
-    let C = "scissors"
 
     let randomNumber = Math.floor(Math.random()*3)
 
     // Map each number to a variable
     switch(randomNumber){
         case 0:
-            return A;
+            return "rock";
         case 1:
-            return B;
+            return "paper";
         case 2:
-            return C;
+            return "scissors";
     }
 }
 
